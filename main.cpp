49c10af6376f198a811e09d14ad55785706e1d62
Álvaro Antônio <wiredista@gmail.com>
2020-05-16 12:41:12 -0300

    Entrada de dados adicionada

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	float rendaSal, rendaServ, rendaCap, gastMed, gastEdu;
+
+	cout << "Renda anual com salário: ";
+	cin >> rendaSal;
+	cout << "Renda anual com prestação de serviço: ";
+	cin >> rendaServ;
+	cout << "Renda anual com ganho de capital: ";
+	cin >> rendaCap;
+	cout << "Gastos médicos: ";
+	cin >> gastMed;
+	cout << "Gastos educacionais: ";
+	cin >> gastEdu;
+
+	// Debug!!
+	//	cout << rendaSal << rendaServ << rendaCap << gastMed << gastEdu << endl;
+}

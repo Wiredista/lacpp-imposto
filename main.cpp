40b5b9f3b1818b42d55c151dea5bbeb933e7725a
Álvaro Antônio <wiredista@gmail.com>
2020-05-16 13:22:36 -0300

    Relatorio gerado

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
-#include <stdio.h>
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 
 int main() {
 	float rendaSal, rendaServ, rendaCap, gastMed, gastEdu;
-
+	float iSal, iServ, iCap, ded, abat;
 	cout << "Renda anual com salário: ";
 	cin >> rendaSal;
 	cout << "Renda anual com prestação de serviço: ";
@@ -18,6 +16,27 @@ int main() {
 	cout << "Gastos educacionais: ";
 	cin >> gastEdu;
 
-	// Debug!!
-	//	cout << rendaSal << rendaServ << rendaCap << gastMed << gastEdu << endl;
+	cout << fixed << setprecision(2);
+
+	// Calculo imposto
+	iSal = (rendaSal/12 < 3000) ? 0 : (rendaSal/12 < 5000) ? 0.1 * rendaSal : 0.2 * rendaSal;
+	iServ = 0.15 * rendaServ;
+	iCap = 0.2 * rendaCap;
+	ded = 0.3 * (iSal + iServ + iCap);
+	abat = ((0.3 * (iSal + iServ + iCap) > ded) ? ded : (0.3 * (iSal +iServ + iCap)));
+	// -- END --
+
+	cout << "\nRELATÓRIO DE IMPOSTO DE RENDA \n\nCONSOLIDADO DE RENDA:\n";
+	cout << "Imposto sobre salário: " << iSal << endl;
+	cout << "Imposto sobre serviços: " << iServ << endl;
+	cout << "Imposto sobre ganho de capital: " << iCap << endl;
+
+	cout << "\n\nDEDUÇÕES:\n";
+	cout << "Máximo dedutível: " << ded << endl;
+	cout << "Gastos dedutiveis: " << gastMed + gastEdu << endl;
+
+	cout << "\nRESMO:\n";
+	cout << "Imposto bruto total: " << iSal + iServ + iCap << endl;
+	cout << "Abatimento: " << abat << endl;
+	cout << "Imposto Devido: " << (iSal + iServ + iCap) - abat << endl;
 }
